d236fcab729068b21c09526ed12fc7e6d4becb82
Austin Walela <austinmasibo@gmail.com>
2020-03-27 12:16:13 +0300

    added auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,14 +2,66 @@ const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const db = require('./auth-model');
+const db = require('./auth-model')
 
-router.post('/register', (req, res) => {
-  // implement registration
+const generateToken = user => {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: 60 * 60
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET || 'genericsecret', options)
+}
+
+const validateCredentials = (req, res, next) => {
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    res
+      .status(400)
+      .json({ message: 'Error. Both username and password required' })
+  } else {
+    next()
+  }
+}
+
+router.post('/register', validateCredentials, (req, res) => {
+  const { username, password } = req.body
+  const hashedPassword = bcrypt.hashSync(password, 10)
+
+  db.add({ username, password: hashedPassword })
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json(err)
+    })
 })
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', validateCredentials, (req, res) => {
+  const { username, password } = req.body
+
+  db.findBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials. Try again' })
+      }
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(401).json({ message: err.message }) // funky {}
+    })
 })
 
 module.exports = router
