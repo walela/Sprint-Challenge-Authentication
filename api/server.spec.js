a7a04894345c0201d3eacca8878bbb37d24743ba
Austin Walela <austinmasibo@gmail.com>
2020-03-27 13:23:23 +0300

    added 404 endpoint tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -9,8 +9,18 @@ describe('server', () => {
     })
     it('returns the correct message', async () => {
       const res = await request(server).get('/')
-      console.log(res)
       expect(res.body.message).toBe('API is up!')
+    })
+  })
+
+  describe('[GET] any invalid endpoint', () => {
+    it('returns the correct HTTP status', async () => {
+      const res = await request(server).get('/foo')
+      expect(res.status).toBe(404)
+    })
+    it('returns the correct message', async () => {
+      const res = await request(server).get('/foobar')
+      expect(res.error.text).toBe('Not Found')
     })
   })
 })
